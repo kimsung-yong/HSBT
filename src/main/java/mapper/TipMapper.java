7f1acec8e1d6d16defc5ed0b8a07a7777481caa9
IT <tm120202@naver.com>
2020-02-10 13:30:05 +0900

    200210 13:29 NOH

--- src/main/java/mapper/TipMapper.java
+++ src/main/java/mapper/TipMapper.java
@@ -6,4 +6,7 @@ import java.util.List;
 
 public interface TipMapper {
     public List<TipVO> getList();
+    public void insert(TipVO tipVO);
+    public void insertSelectKey(TipVO tipVO);
+    public TipVO read(Long tNo);
 }
--- src/test/java/mapper/TipMapperTests.java
+++ src/test/java/mapper/TipMapperTests.java
@@ -1,5 +1,6 @@
 package mapper;
 
+import domain.TipVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.junit.Test;
@@ -18,5 +19,33 @@ public class TipMapperTests {
     @Test
     public void testGetList() {
         mapper.getList().forEach(tipVO -> log.info(tipVO));
+    }
+
+    @Test
+    public void testInsert() {
+        TipVO tipVO = new TipVO();
+        tipVO.setTTitle("New Test");
+        tipVO.setTContent("New Test");
+        tipVO.setUNo(1L);
+
+        mapper.insert(tipVO);
+        log.info(tipVO);
+    }
+
+    @Test
+    public void testInsertSelectKey() {
+        TipVO tipVO = new TipVO();
+        tipVO.setTTitle("New Select Key Test");
+        tipVO.setTContent("New Select Key Test");
+        tipVO.setUNo(1L);
+
+        mapper.insertSelectKey(tipVO);
+        log.info(tipVO);
+    }
+
+    @Test
+    public void testRead() {
+        TipVO tipVO = mapper.read(4L);
+        log.info(tipVO);
     }
 }
