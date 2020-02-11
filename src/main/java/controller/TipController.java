e668e74f7e9504247fc7213eccd0b05f0e3b7fea
IT <tm120202@naver.com>
2020-02-11 09:35:18 +0900

    200211 09:35 NOH

--- src/main/java/controller/TipController.java
+++ src/main/java/controller/TipController.java
@@ -2,13 +2,17 @@ package controller;
 
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import service.TipService;
 
 @Controller
 @RequestMapping("/board/tipboard/*")
 @Log4j
 public class TipController {
+    /*private TipService service;*/
+
     @GetMapping("/list")
     public void list() {
 
