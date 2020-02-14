6070c05c0f5243f5dcd0a71e0fd6e062d02a25df
IT <anrl1014@naver.com>
2020-02-14 12:16:30 +0900

    200214 12:16 kim

--- src/main/java/mapper/BoardMapper.java
+++ src/main/java/mapper/BoardMapper.java
@@ -1,6 +1,7 @@
 package mapper;
 
 import domain.BoardVO;
+import domain.Criteria;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -8,10 +9,12 @@ import java.util.List;
 public interface BoardMapper {
 
     public List<BoardVO> getList();
+    public List<BoardVO> getListWithPaging(Criteria cri);
     public BoardVO getListSelect(Long b_no);
     public void insert(BoardVO vo);
     public void update(BoardVO vo);
     public void delete(Long b_no);
+    public int getCount(Criteria cri);
 
 
 
