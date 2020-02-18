59c2d43bc5deba58366ed98984a08e13906ffb1c
IT <tm120202@naver.com>
2020-02-18 15:00:12 +0900

    20/02/18 15:00 NOH

---
+++ src/main/java/domain/notice/NoticeVO.java
@@ -0,0 +1,15 @@
+package domain.notice;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class NoticeVO {
+    private Long n_no;
+    private String n_title;
+    private String n_content;
+    private String id;
+    private Date n_regtime;
+    private Date n_updatetime;
+}
